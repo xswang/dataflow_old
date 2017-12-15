04e07fce7e93cd1316c90dd8ed0b963a3b2d54f7
xswang <2012wxs@gmail.com>
2017-12-15 16:31:17 +0800

    add: main

---
+++ dataflow/core/graph/main.cc
@@ -0,0 +1,15 @@
+/*
+ * main.cc
+ * Copyright (C) 2017 XiaoshuWang <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "core/base/class_register.h"
+
+int main() {
+  
+  return 0;
+}
+
+
