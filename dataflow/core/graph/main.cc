bfab751c760030af7bec66ea737b5c1a258ed956
xswang <2012wxs@gmail.com>
2017-12-22 14:14:00 +0800

    update: test sucess

--- dataflow/core/graph/main.cc
+++ dataflow/core/graph/main.cc
@@ -9,13 +9,19 @@
 #include "core/ops/operator.h"
 #include "core/ops/conv.h"
 #include "core/proto/dataflow.pb.h"
+#include "core/graph/proto_parse.h"
 
 int main() {
   dataflow::Conv* conv;
   conv = dynamic_cast<dataflow::Conv*>(dataflow::CREATE_OP("conv"));
   std::cout << conv->get_op_name() << std::endl;
-  std::ifstream in("models/googlenet_model_complete.txt", std::ios::in);
-  //dataflow::GraphDef graph_def;
+  //std::ifstream in("models/googlenet_model_complete.txt", std::ios::in);
+  dataflow::GraphDef graph_def;
+  //dataflow::NodeDef node_def;
+  const char* model = "/root/xiaoshu/dataflow/dataflow/models/googlenet_model_complete.txt";
+  dataflow::PbParse* pb;
+  bool res = pb->ReadProtoFromTextFile(model, &graph_def);
+  std::cout << graph_def.node_def_size() << std::endl;
   return 0;
 }
 
