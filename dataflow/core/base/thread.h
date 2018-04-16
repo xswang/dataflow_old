51eed5d58432b48ec494285b2ac624c7dcd457be
xswang <2012wxs@gmail.com>
2018-04-16 21:52:57 +0800

    add: thread.h

---
+++ dataflow/core/base/thread.h
@@ -0,0 +1,13 @@
+/*
+ * thread.h
+ * Copyright (C) 2018 XiaoshuWang <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef THREAD_H
+#define THREAD_H
+
+
+
+#endif /* !THREAD_H */
