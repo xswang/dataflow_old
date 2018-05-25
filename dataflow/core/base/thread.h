878b98b0592c608755844b6c077ee94d90cea860
xswang <2012wxs@gmail.com>
2018-05-25 10:00:59 +0800

    update: thread.h

--- dataflow/core/base/thread.h
+++ dataflow/core/base/thread.h
@@ -7,7 +7,15 @@
 
 #ifndef THREAD_H
 #define THREAD_H
-
-
+namespace dataflow {
+template <typename ThreadType>
+class ThreadPool {
+ public: 
+  ThreadPool(size_t thread_num);
+  ~ThreadPool();
+ private:
+  std::vector<ThreadType> workers_;
+};
+}
 
 #endif /* !THREAD_H */
